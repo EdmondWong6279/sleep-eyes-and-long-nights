d5fb7afe037dd09410d661d14c91223c52c4768b
ed6279 <edmondwong6279@gmail.com>
2018-01-12 10:47:55 +0000

    10:47

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -1,48 +1,46 @@
+/** Recursion.java makes a pretty tree thing.
+ *
+ *  @author Ed Wong
+ *  @version 12/1/2018
+ */
+
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Recursion extends JPanel{
-	private int length;
 	private static final int WIDTH = 400;
 	private static final int HEIGHT= 400;
-	private int xcoord=WIDTH/2;
-	private int ycoord=HEIGHT/2;
+	private double angle = Math.PI/6.0;
+	private int length;
 
 	public Recursion(int length){
 		this.length=length;
 	}
-
-	public String rule1(char char1){
-		if (char1 == 'b')
-			return "ab";
-		return Character.toString(char1);
-	}
-
-	public String rule2(char char2){
-		if (char2 == 'a')
-			return "ab";
-		return Character.toString(char2);
-	}
-
-	public void generate() {
-		for (int i=0; i<sentence.length; i++){
-			
-		}
-	}
-
+	
 	@Override
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		branch(length, g);
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.translate(WIDTH/2,HEIGHT);
+		branch(length, g2d);
 	}
 
-	public static void branch(int length, Graphics g){
-		//start
-		g.drawLine(0,400,10,12);
-		if (length>0){
-			//TODO rotate coords
+	//Use translate instead
+	public void branch(int length, Graphics2D g2d){
+		g2d.drawLine(0, 0, 0, -length);
+		if(length>10) {
+			AffineTransform transform = g2d.getTransform();
+			length = (int) Math.round(length*0.67);
+			g2d.translate(0,-length);
+			g2d.rotate(angle);
+			branch(length, g2d);
+			g2d.setTransform(transform);
+			g2d.rotate(-2*angle);
+			branch(length, g2d);
 		}
 	}
 
@@ -50,7 +48,7 @@ public class Recursion extends JPanel{
 		JFrame frame = new JFrame("Dragon Curve in Java");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		Recursion fractel = new Recursion(5);
+		Recursion fractel = new Recursion(HEIGHT/3);
 		frame.add(fractel);
 		frame.setSize(WIDTH,HEIGHT);
 		frame.setVisible(true);
