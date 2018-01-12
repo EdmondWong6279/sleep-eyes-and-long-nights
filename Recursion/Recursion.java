cd1aeea7ca07cba2a67032d52928fd6f8a53d638
ed6279 <edmondwong6279@gmail.com>
2018-01-12 12:35:45 +0000

    Prelecture cram

--- Recursion/Recursion.java
+++ Recursion/Recursion.java
@@ -7,15 +7,17 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class Recursion extends JPanel{
-	private static final int WIDTH = 400;
-	private static final int HEIGHT= 400;
-	private double angle = Math.PI/6.0;
+	private static final int WIDTH = 800;
+	private static final int HEIGHT= 600;
+	private double angle = Math.PI/2.0;
 	private int length;
+	private int counter=0;
 
 	public Recursion(int length){
 		this.length=length;
@@ -31,19 +33,21 @@ public class Recursion extends JPanel{
 
 	public void branch(int length, Graphics2D g2d){
 		g2d.drawLine(0, 0, 0, -length);
-		if(length>10) {
+		if(counter<10) {
 			g2d.translate(0,-length);
 			g2d.rotate(angle);
-			length = (int) Math.round(length*0.67);
 			branch(length, g2d);
+			g2d.rotate(-2.0*angle);
+			branch(length, g2d);
+			counter++;
 		}
 	}
 
 	public static void main(String[] args){
-		JFrame frame = new JFrame("Dragon Curve in Java");
+		JFrame frame = new JFrame("Fractal Tree in Java");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		Recursion fractel = new Recursion(HEIGHT/3);
+		Recursion fractel = new Recursion(HEIGHT/2);
 		frame.add(fractel);
 		frame.setSize(WIDTH,HEIGHT);
 		frame.setVisible(true);
